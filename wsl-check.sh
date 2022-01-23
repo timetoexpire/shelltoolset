dea8b66b654fd2de848f5cac8ee084515ba73194
timetoexpire <github-1809@timetoexpire.co.uk>
2022-01-23 14:34:16 +0000

    it now able to distinguish between versions of WSL1/WSL2. also some examples of how use it

--- wsl-check.sh
+++ wsl-check.sh
@@ -1,20 +1,66 @@
 #!/bin/bash
-# $CheckFor_WSL is not NULL then using Microsoft Windows WSL2
-CheckFor_WSL=$(grep "microsoft-standard-WSL" /proc/version)
 
-echo "cat /proc/version [$(cat /proc/version)]"
-echo "String output for \$CheckFor_WSL [$CheckFor_WSL]"
+## Copyright timetoexpire.co.uk 2022 (MIT License) https://github.com/timetoexpire/shelltoolset
+## Returns result using $CheckFor_WSL 
+## "" (NULL) is not using WSL, 
+## "0" is unknown version of WSL
+## "1" is WSL1
+## "2" is WSL2
 
-# If want to check WSL is not true, else it is using WSL
+CheckFor_WSL="/proc/sys/fs/binfmt_misc/WSLInterop"
+if [ -f "$CheckFor_WSL" ]; then
+  CheckFor_WSL="/proc/version"
+  case "$(cat $CheckFor_WSL)" in
+    *"-Microsoft ("*)
+      CheckFor_WSL="1" # This is value for WSL1
+      ;;
+    *"-microsoft-standard-WSL2 ("*)
+      CheckFor_WSL="2" # This is value for WSL2
+      ;;
+    *)
+      CheckFor_WSL="0" # This is value if WSL version in unknown
+      ;;
+  esac
+else
+  CheckFor_WSL="" # This value NULL if WSLInterop dosnt exists
+fi
+
+## That all is need to test for WSL, below is example of how to use it
+
+echo "CheckFor_WSL result [$CheckFor_WSL]"
+
+echo "TEST 1 (case)"
+case "$CheckFor_WSL" in
+  "")
+    echo "File (WSLInterop) dosnt exist so it not WSL!"
+    ;;
+  "0")
+    echo "Unknown version of WSL?????"
+    ;;
+  "1")
+    echo "WSL1"
+    ;;
+  "2")
+    echo "WSL2"
+    ;;
+esac
+
+echo " - -"
+
+echo "TEST 2 (if)"
 if [ -z "$CheckFor_WSL" ]; then
-  echo "[-z] Not using WSL"
-else
-  echo "[-z] WSL in use"
+  echo "File (WSLInterop) dosnt exist so it not WSL!"
+fi
+if [ ! -z "CheckFor_WSL" ]; then
+  echo "The system is using WSL, not showing version"
+fi
+if [ "$CheckFor_WSL" = "0" ]; then
+   echo "Unknown version of WSL?????"
+fi
+if [ "$CheckFor_WSL" = "1" ]; then
+   echo "WSL1"
+fi
+if [ "$CheckFor_WSL" = "2" ]; then
+   echo "WSL2"
 fi
 
-# If want to check WSL is true, else it not using WSL
-if [ ! -z "$CheckFor_WSL" ]; then
-  echo "[! -z] WSL in use"
-else
-  echo "[! -z] Not using WSL"
-fi
