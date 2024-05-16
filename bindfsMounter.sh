c4b011e102bfecbfb9fb5beff3f5dd6b7507d292
timetoexpire <64891214+timetoexpire@users.noreply.github.com>
2024-05-16 13:50:24 +0100

    Create bindfsMounter.sh
    This is useful when need to access folders that are root, when can not access as root.
    
    IE Visual Studio Code, SSH

---
+++ bindfsMounter.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+## Copyright timetoexpire.co.uk 2022 (MIT License) https://github.com/timetoexpire/shelltoolset
+
+function IsMounted() {
+  local checking="$1"
+  local basePath
+  if [ $# -eq 1 ]; then
+    basePath="$PWD"
+  else
+    basePath="$2"
+  fi
+  local checkingPath="$basePath""/""$checking"
+#  echo "Checking '$checking' Path '$checkingPath'"
+
+  if mount | grep -q "$checkingPath"; then
+#    echo "True"
+    return 0
+  else
+#    echo "False"
+    return 1
+  fi
+}
+
+if [ $# -lt 2 ]; then
+  echo "Error No Pars [$*]"
+  echo "[BindMap] [BindName]"
+  echo "[BindMap] [BindName] [BindPath]"
+  echo "sudo bindfs --map=root/${USER}:@root/@$(id -gn) \"\$BindMap\" \"\$BindPath/\$BindName\""
+#sudo bindfs --map=root/${USER}:@root/@$(id -gn) "$bindMap" "$bindFullPath"
+  exit 1
+fi
+
+if [ $# -lt 3 ]; then
+  bindPath="/home/$USER/.bind-mounts"
+else
+  bindPath="$3"
+  if [[ "$bindPath" == */ ]]; then
+#    echo -n "Found bindPath [$bindPath]"
+    bindPath="${bindPath%/}"
+#    echo " fixed [$bindPath]"
+  fi
+fi
+
+bindName="$2"
+
+bindMap="$1"
+
+if [[ "$bindMap" == */ ]]; then
+#  echo -n "Found bindMap [$bindMap] "
+  bindMap="${bindMap%/}"
+#  echo "Fixed [$bindMap]"
+fi
+
+bindFullPath="$bindPath""/""$bindName"
+
+#echo "BindMap = $bindMap"
+#echo "BindName = $bindName"
+#echo "BindPath = $bindPath"
+#echo "BindFullPath = $bindFullPath"
+
+IsMounted "$bindName" "$bindPath"
+
+if [ $? -eq 1 ]; then
+#  echo "Seting up"
+  if [ -d "$bindFullPath" ]; then
+    echo "Remove all contents in directory $bindFullPath"
+    sudo rm -R -v "$bindFullPath"
+  fi
+  mkdir -p "$bindFullPath"
+  sudo bindfs --map=root/${USER}:@root/@$(id -gn) "$bindMap" "$bindFullPath"
+  echo -n "bindfs set up as "
+else
+  echo -n "Already exists "
+fi
+
+echo "BindMap=$bindMap BindName=$bindName BindFullPath=$bindFullPath"
+
+
